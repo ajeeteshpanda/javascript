3a08df7e19ca10786ba840bda306460d879a6bc0
ajeeteshpanda <ajeeteshpanda20@gmail.com>
2025-05-23 20:53:11 +0530

    added calculateTime js file

---
+++ internalTesting/calculatetime.js
@@ -0,0 +1,15 @@
+// Define two variables
+let a = 10;
+let b = 5;
+
+// Perform calculations
+let sum = a + b;
+let difference = a - b;
+let product = a * b;
+let quotient = a / b;
+
+// Display results
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
